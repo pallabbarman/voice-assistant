85fd4440d22e61c6d0fc5ef63a4c35df988408a0
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-15 16:23:44 +0600

    take command succesfully

--- voiceAssistant.py
+++ voiceAssistant.py
@@ -1,8 +1,10 @@
 import pyttsx3
+import speech_recognition as sr
+import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-print(voices[1].id)
+# print(voices[1].id)
 engine.setProperty('voice', voices[1].id)
 
 
@@ -11,5 +13,41 @@ def speak(audio):
     engine.runAndWait()
 
 
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 0 and hour < 12:
+        speak("Good Morning!")
+
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("Good Evening!")
+
+    speak("I am Mikasa Sir. Please tell me how can I help you")
+
+
+def takeCommand():
+    # input will be take from microphone and return as a string in output
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-us')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+
+    return query
+
+
 if __name__ == "__main__":
-    speak("Pallab is good boy")
+    wishMe()
+    takeCommand()
