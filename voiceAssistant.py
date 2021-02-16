8705924d72791a01b3bd925770985ee7405425fc
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-02-16 16:03:05 +0600

    added google and stackoverflow

--- voiceAssistant.py
+++ voiceAssistant.py
@@ -65,3 +65,9 @@ if __name__ == "__main__":
 
         elif 'open youtube' in query:
             webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
