bb6908282c10ffd4f53ddfc9e4ceda7f969431c2
Pallab Barman <76774117+pallabbarman@users.noreply.github.com>
2021-03-26 11:36:59 +0600

    map,time,date,music added

--- voiceAssistant.py
+++ voiceAssistant.py
@@ -6,6 +6,7 @@ import webbrowser
 import os
 import pyjokes
 import pywhatkit
+import random
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -17,18 +18,37 @@ def speak(audio):
     engine.runAndWait()
 
 
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M:%S")
+    speak("The current time is")
+    speak(Time)
+
+
+def date():
+    year = datetime.datetime.now().year
+    month = datetime.datetime.now().month
+    date = datetime.datetime.now().day
+    speak("The current date is")
+    speak(date)
+    speak(month)
+    speak(year)
+
+
 def wishMe():
+    speak("Welcome sir")
+    time()
+    date()
     hour = int(datetime.datetime.now().hour)
-    if hour >= 0 and hour < 12:
+    if 0 <= hour < 12:
         speak("Good Morning!")
 
-    elif hour >= 12 and hour < 18:
+    elif 12 <= hour < 18:
         speak("Good Afternoon!")
 
     else:
         speak("Good Evening!")
 
-    speak("I am Emma Sir. Please tell me how can I help you")
+    speak("I am Emma. Please tell me how can I help you")
 
 
 def takeCommand():
@@ -52,10 +72,18 @@ def takeCommand():
 
 if __name__ == "__main__":
     wishMe()
-    # while True:
-    if 1:
+    while True:
         query = takeCommand().lower()
-        if 'wikipedia' in query:
+        if 'time' in query:
+            time()
+
+        elif 'date' in query:
+            date()
+
+        elif 'how are you' in query:
+            speak("I am fine, Sir thanks for asking.")
+
+        elif 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=2)
@@ -71,14 +99,40 @@ if __name__ == "__main__":
             webbrowser.get(
                 "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open("google.com")
 
-        elif 'music' in query:
-            music_dir = 'F:/Audio'
-            songs = os.listdir(music_dir)
-            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'play songs' in query:
+            video = 'F:/Video/Song'
+            audio = 'F:/Audio'
+            speak("What songs should I play? Audio or Video")
+            ans = (takeCommand().lower())
+            while (ans != 'audio' and ans != 'video'):
+                speak("I could not understand you. Please Try again.")
+                ans = (takeCommand().lower())
 
-        elif 'time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Sir, the time is {strTime}")
+            if 'audio' in ans:
+                songs_dir = audio
+                songs = os.listdir(songs_dir)
+                print(songs)
+
+            elif 'video' in ans:
+                songs_dir = video
+                songs = os.listdir(songs_dir)
+                print(songs)
+
+            speak("select a random number")
+            rand = (takeCommand().lower())
+            while ('number' not in rand and rand != 'random'):
+                speak("I could not understand you. Please Try again.")
+                rand = (takeCommand().lower())
+
+            if 'number' in rand:
+                rand = int(rand.replace("number ", ""))
+                os.startfile(os.path.join(songs_dir, songs[rand]))
+                continue
+
+            elif 'random' in rand:
+                rand = random.randint(1, 200)
+                os.startfile(os.path.join(songs_dir, songs[rand]))
+                continue
 
         elif 'open code' in query:
             codePath = "C:\\Microsoft VS Code\\Code.exe"
@@ -94,6 +148,20 @@ if __name__ == "__main__":
             speak('playing...'+song)
             pywhatkit.playonyt(song)
 
-        elif 'search' in query:
-            search = 'https://www.google.com/search?q=' + query
-            webbrowser.open(search)
+        elif 'search google' in query:
+            speak("What should I search?")
+            search = takeCommand().lower()
+            speak('Searching...')
+            webbrowser.open('https://www.google.com/search?q=' + search)
+
+        elif "where is" in query:
+            query = query.replace("where is", "")
+            location = query
+            speak("User asked to Locate")
+            speak(location)
+            webbrowser.open(
+                "https://www.google.com/maps/place/" + location + "")
+
+        elif 'offline' in query:
+            speak("going Offline")
+            quit()
